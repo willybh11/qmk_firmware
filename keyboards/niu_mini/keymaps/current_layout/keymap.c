ced3d77673a676ae41303d5f79dabe9a090070ef
willybh11 <willhagele@gmai.com>
2020-06-20 12:22:56 -0400

    Add new key

--- keyboards/niu_mini/keymaps/current_layout/keymap.c
+++ keyboards/niu_mini/keymaps/current_layout/keymap.c
@@ -10,32 +10,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Enter |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | Ctrl |  GUI | Caps |  Alt |Layer1|    Space    |Layer2|  /   | Left | Down |Right |
+     * | Ctrl |  GUI | Caps |  Alt |Layer1|      |Space |Layer2|  /   | Left | Down |Right |
      * `-----------------------------------------------------------------------------------'
      */
-    [0] = LAYOUT_planck_mit(
+    [0] = LAYOUT_ortho_4x12(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
-        KC_LCTL, KC_LGUI, KC_CAPS, KC_LALT, MO(1),     KC_SPC,     MO(2), KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_CAPS, KC_LALT, MO(1), _______,KC_SPC, MO(2), KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /* Layer 1
      * ,-----------------------------------------------------------------------------------.
      * |   `  |      | prev | play | next |   (  |   )  |      |      |      |      |      |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * |      |      | mute | Vol- | Vol+ |   [  |   ]  |   -  |   +  |      |      |   \  |
+     * |      |      | mute | Vol- | Vol+ |   [  |   ]  |   -  |   =  |      |      |   \  |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |      |      |      |      |      |   {  |   }  |      |      |      |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Reset|      |      |      |      |      |      |      |      |      |      |      |
      * `-----------------------------------------------------------------------------------'
      */
-    [1] = LAYOUT_planck_mit(
+    [1] = LAYOUT_ortho_4x12(
         KC_GRV,  _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_LPRN, KC_RPRN, _______, _______, _______, _______, _______,
         _______, _______, KC_MUTE, KC_VOLU, KC_VOLD, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  _______, _______, KC_BSLS,
         _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______, _______, _______, _______,
-        RESET,   _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
+        RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
     /* Layer 2 (r_ Indicates RGB Controls)
@@ -49,11 +49,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |r_bri+|r_bri-|      |      |      |      |      |      |   0  |      |      |      |
      * `-----------------------------------------------------------------------------------'
      */
-    [2] = LAYOUT_planck_mit(
+    [2] = LAYOUT_ortho_4x12(
         RGB_TOG, RGB_MOD, KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, KC_7,    KC_8,    KC_9,    _______,_______,
         RGB_HUI, RGB_HUD, KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______, KC_4,    KC_5,    KC_6,    _______, _______, 
         RGB_SAI, RGB_SAD, KC_F1,   KC_F2,   KC_F3,   KC_F12,  _______, KC_1,    KC_2,    KC_3,    _______,_______,
-        RGB_VAI, RGB_VAD, _______, _______, _______,     _______,      _______, KC_0,    _______, _______, _______
+        RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______, _______, KC_0,    _______, _______, _______
     )
 };
 
